5ce281436ec2c6ae19f02748e3016c10004bd35c
Frédéric NOËL <contact@fredericnoel.com>
2022-06-27 12:30:39 +0200

    Importance de la position de l'opération d'incrémentation

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php 
 
+$truc = 20;
+echo $truc++;
+echo "<br />";
+echo $truc;
+echo "<br />";
+
 $x = 210;
 
 // Selon PSR-12 : mettre des espaces après if et elseif
-if ($x > 0) {
-    echo '$x est positif';
-} elseif ($x < 0) {
-    echo '$x est negatif';
+
+// if ($x)
+if (isset($x) && is_numeric($x)) {
+    if ($x > 0) {
+        echo '$x est positif';
+    } elseif ($x < 0) {
+        echo '$x est negatif';
+    } else {
+        echo '$x est nul';
+    }
+
 } else {
-    echo '$x est nul';
+    echo '$x n\'esiste pas';
 }
+
+
