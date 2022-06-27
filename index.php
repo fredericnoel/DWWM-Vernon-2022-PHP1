3d1f131e7a5008b9df204b5ad2bbb17cc112c3cc
Frédéric NOËL <contact@fredericnoel.com>
2022-06-27 11:42:46 +0200

    Test if, elseif, else

--- index.php
+++ index.php
@@ -1,34 +1,12 @@
 <?php 
 
-/* Opérateurs
-- addition +
-- soustraction -
-- multiplication *
-- division /
-- modulo %
-- ** (10 puissance 2 : 10 ** 2)
-*/
+$x = 210;
 
-/*
-Opérateurs combinés 
-$i = $i + 1;
-$i++;
-$i += 1;
-*/
-
-/*
-Opérateurs de comparaison
-- == compare la valeur
-- === compare la valeur et le type
-- != ou <> différent en valeurs
-- !== différent en valeurs et en types
-- <
-- >
-- <=
-- =>
-
-*/
-
-// Exemple de vérification d'opérateur de comparaison avec la fonction var_dump()
-$x= 42;
-var_dump($x == "42");
+// Selon PSR-12 : mettre des espaces après if et elseif
+if ($x > 0) {
+    echo '$x est positif';
+} elseif ($x < 0) {
+    echo '$x est negatif';
+} else {
+    echo '$x est nul';
+}
