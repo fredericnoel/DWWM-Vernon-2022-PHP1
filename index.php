b8ea81840ade9722d774a0a8be3e4574c1cde36b
Frédéric NOËL <contact@fredericnoel.com>
2022-06-27 14:19:56 +0200

    Exemple balises dl, dt, dd

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
-<?php 
-
-$toto = 0;
-$resultat = "<ul>";
-
-do {   
-    $resultat .= "<li>";
-    $resultat .= $toto;
-    $resultat .= "</li>";
-    $toto++;
-} while ($toto < 10);
-
-$resultat .= "</ul>";
-echo $resultat;
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fraise</title>
+</head>
+<body>
+    <dl>
+        <dt>Fraise</dt>
+        <dd>Fruit rouge dont la partie comestible est un réceptacle de la fleur devenu charnu et sucré après la fécondation et qui porte les akènes</dd>
+        <dd>Membrane enveloppant les intestins du veau ou de l’agneau, ou bien intestin grêle de veau, ouvert, lavé et poché à l’eau bouillante.</dd>
+    </dl>
+</body>
+</html>
