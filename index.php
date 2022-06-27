f3fe062936c82bcb6af7c37f4de5d5e521366234
Frédéric NOËL <contact@fredericnoel.com>
2022-06-27 16:21:16 +0200

    Fonction basique avec instruction echo

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fraise</title>
+    <title>PHP - Fonctions</title>
 </head>
 <body>
-    <dl>
-        <dt>Fraise</dt>
-        <dd>Fruit rouge dont la partie comestible est un réceptacle de la fleur devenu charnu et sucré après la fécondation et qui porte les akènes</dd>
-        <dd>Membrane enveloppant les intestins du veau ou de l’agneau, ou bien intestin grêle de veau, ouvert, lavé et poché à l’eau bouillante.</dd>
-    </dl>
+    <?php
+        function salut() {
+            echo "Salut";
+        }
+
+        salut();
+    ?>
 </body>
 </html>
