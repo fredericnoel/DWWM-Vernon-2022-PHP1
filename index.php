129850794f84aeb8c16ee756bb2706198906eed4
Frédéric NOËL <contact@fredericnoel.com>
2022-06-27 14:02:01 +0200

    Exercice boucle for avec compteur

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php 
 
+$compteur = 0;
 $resultat = "<ul>";
-for($i = 0 ; $i <= 100 ; $i++) {
-    $resultat .= "<li>";
-    $resultat .= $i;
-    $resultat .= "</li>";
+for($i = -17627 ; $i <= 14256 ; $i++) {
+    if ($i % 7 === 0) {
+        $resultat .= "<li>";
+        $resultat .= $i;
+        $resultat .= "</li>";
+        $compteur++;
+    }
 }
-$resultat .= "<ul>";
+$resultat .= "</ul>";
+$resultat .= "<p>Il y a $compteur resultats</p>";
 
 echo $resultat;
