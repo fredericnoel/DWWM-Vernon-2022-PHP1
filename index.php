149be9a79e93c95e2fc69383e577d3bcd95cb675
Frédéric NOËL <contact@fredericnoel.com>
2022-06-28 14:17:49 +0200

    Exemple syntaxe Heredoc

--- index.php
+++ index.php
@@ -17,14 +17,25 @@ define("TVATAUXNORMAL", 0.22);
 <body>
     <?php
 
-    function listerFruits(...$fruits) {
-        foreach($fruits as $value) {
-            echo "<p>Ce est un ou une $value </p>";
-        }
-    }
+    // Syntaxe Heredoc
 
-    listerFruits("Banane", "Pomme", "Poire");
+    $texte =<<<michelestalaplage
+    Cupcake ipsum dolor sit amet I love I love cheesecake.
+    Croissant sweet danish I love powder. Donut chocolate jelly-o toffee I love.
+    Toffee dessert sweet roll liquorice pastry ice cream.
+    Gummies dessert marshmallow liquorice lollipop wafer.
+    Jelly I love powder shortbread lollipop cake I love.
+    I love bonbon brownie pastry shortbread macaroon sweet roll tart cotton candy.
+    Sweet chocolate gingerbread pudding sweet tart I love soufflé.
+    Tootsie roll sesame snaps tootsie roll I love toffee fruitcake jelly-o.
+    Jelly gingerbread cookie gummies carrot cake toffee croissant.
+    Gummies muffin sweet roll halvah topping shortbread.
+    Pie sesame snaps toffee sweet roll I love I love I love marzipan.
+    michelestalaplage;
 
+    echo "<pre>";
+    echo $texte;
+    echo "</pre>";
     ?>
 </body>
 </html>
